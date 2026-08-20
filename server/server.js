d303b222f872262515fb56a2b15139f20d7428a1
sk-jadoon <jsidra845@gmail.com>
2026-08-20 21:23:58 +0500

    Fix Vercel backend serverless deployment

--- server/server.js
+++ server/server.js
@@ -1,20 +1,5 @@
 require("dotenv").config();
 
 const app = require("./app");
-const { connectDB } = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Server startup failed:", error);
-  }
-};
-
-startServer();
+module.exports = app;
